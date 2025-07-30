a012ca936fa5a2bc1963f8dd35aa2fa9d092ba1e
Vaishakh A <vaishakh.cs22@sahyadri.edu.in>
2025-07-30 23:31:17 +0530

    DAY 15

---
+++ DAY15/Integer_Finder.java
@@ -0,0 +1,20 @@
+package com.demo.DAY15;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Integer_Finder {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(
+			    4, 5, 6, 5, 4, 7, 5, 6, 4, 8, 9, 4
+			));
+		Map<Integer, Integer> freqMap = new HashMap<>();
+		
+
+
+	}
+
+}
